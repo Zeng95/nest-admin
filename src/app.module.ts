61aad91809216d2b0fa3318cb5e977fc2488ae6b
Leo Zeng <qingchun1995@gmail.com>
2021-11-28 10:59:22 +0800

    feat(auth): :sparkles: create an auth controller and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './users/users.module';
 
 @Module({
-  imports: [UserModule, TypeOrmModule.forRoot(), AuthModule],
+  imports: [TypeOrmModule.forRoot(), UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService]
 })
